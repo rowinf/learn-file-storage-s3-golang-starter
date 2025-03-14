de811266b703841a2c500a91221f83fa03c61470
rowinf <964049+rowinf@users.noreply.github.com>
2025-03-15 09:17:06 +1300

    handle thumbnail upload

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const MaxUploadSize = 10 << 20
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -33,5 +36,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	if err := r.ParseMultipartForm(MaxUploadSize); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing form", err)
+		return
+	}
+	if file, fheader, err := r.FormFile("thumbnail"); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing form", err)
+		return
+	} else {
+		mediaType := fheader.Header["Content-Type"]
+		if data, err := io.ReadAll(file); err != nil {
+			respondWithError(w, http.StatusBadRequest, "Error parsing form", err)
+			return
+		} else {
+			//
+			if video, err := cfg.db.GetVideo(videoID); err != nil {
+				respondWithError(w, http.StatusBadRequest, "Video Not Found", err)
+				return
+			} else {
+				if user, err := cfg.db.GetUser(userID); err != nil {
+					http.Error(w, "User Not Found", http.StatusBadRequest)
+					return
+				} else {
+					if user.ID != video.UserID {
+						respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+						return
+					}
+					// authorized
+					videoThumbnails[video.ID] = thumbnail{
+						data:      data,
+						mediaType: mediaType[0],
+					}
+					url := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", video.ID)
+					video.ThumbnailURL = &url
+					if err := cfg.db.UpdateVideo(video); err != nil {
+						respondWithError(w, http.StatusBadRequest, "Error saving video", err)
+						return
+					}
+					if vid, err := cfg.db.GetVideo(video.ID); err != nil {
+						respondWithError(w, http.StatusBadRequest, "Error saving video", err)
+					} else {
+						respondWithJSON(w, http.StatusOK, vid)
+					}
+				}
+			}
+		}
+	}
 }
